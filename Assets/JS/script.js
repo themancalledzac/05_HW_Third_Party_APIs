4882474ce01fee976329728a92e27442b7a19d66
Zac Edens <themancalledzac@gmail.com>
2020-11-08 15:11:50 -0800

    trying to get the click to save data from the textarea.

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -4,7 +4,7 @@
 // Get the current hour of the day using moment.js
 var currentMoment = moment();
 // var currentDay = moment().format('dddd, MMMM Do'); $("#currentDay").text(currentDay);
-    // ------------------ below code is cleaner--------------------------------------------------//
+// ------------------ below code is cleaner--------------------------------------------------//
 $("#currentDay").text(currentMoment.format('dddd, MMMM Do'));
 var currentHour = parseInt(currentMoment.format("H"));
 console.log( currentHour );
@@ -14,46 +14,60 @@ console.log( currentHour );
 
 // FOR Loop over the hours of the day from (9am to 5pm) i = 9 to i <= 17 ------------------------//
 for (let i = 9; i <= 17; i++) {
-        //---------------DIV ID = '#hour-" + 1;"-------------------------------------------------//
-    
+    //---------------DIV ID = '#hour-" + 1;"-------------------------------------------------//
     var elementId = $("#hour-" + i);
     $(elementId).attr('data-key', i);
-        //---------------DIV ID 
+    //---------------DIV ID 
     if (i < currentHour) {
         $(elementId).addClass("past");
-
+        
     } if (i > currentHour) {
         $(elementId).addClass("future");
     } 
     if (i === currentHour) {
         $(elementId).addClass("present");
     }
-        //      Option A - Select matching element - $( ElementID ).css( "background-color")
-        //      Option B - Select matching element - $( ElementID ).addClass( "is-past")
-        console.log(elementId);
-    };
+    //      Option A - Select matching element - $( ElementID ).css( "background-color")
+    //      Option B - Select matching element - $( ElementID ).addClass( "is-past")
+    console.log(elementId);
+};
 
 
-// event listener on button
-// pull out value of the SIBLING textarea jquery .val
-// setItem to loccal storage to the data-key from that div, with text as value
+// local storage
+// for (let notesIndex = 9; notesIndex <=17; notesIndex++) {
+//     let schedule = localStorage.getItem('hour-' + notesIndex);
+//     $('#hour${notesIndex}').text(schedule);
+//     if ($('#hours{notesIndex}').text() === null) {
+//         $('#hour${notesIndex}').text().empty();
+//     }
+// };
 
-// when page is LOADED (loop) pull data from the 
+// event listener on button PARENT ELEMENT '.Container'
+var btnContainerEl = $('.container');
+    btnContainerEl.on('click', '.saveBtn', saveToStorage);
+    
+    
+function saveToStorage(event) {
 
+    event.preventDefault();
 
+        // pull out value of the SIBLING textarea jquery .val
 
+        // setItem to local storage to the data-key from that div, with text as value
 
+        // when page is LOADED (loop) pull data from the data-keys?
+        
+        // THEN I need to save the associated text for the associated hour.description
+        //  GET the current hour from the 'event.target' (The clicked button)
+        
+    let theClickedHour = $(this).parent('div').children('textarea').data('hour');
+        console.log(theClickedHour);
+        
+        //  > var value = $(event.target).prev().val()
+    let value = $(this).parent('div').children('textarea').val();
+        
+        //  > localStorage.setItem( "hour-"+ theClickedHour, value );
+    localStorage.setItem('hour: ' + theClickedHour, value);
+    console.log(localStorage);
+};
 
-// Add a click event ON A PARENT ELEMENT that can listen
-// to my save button clicks
-
-
-// WHEN the user clicks a save button
-// THEN I need to save the associated text for the associated hour.description
-//  GET the current hour from the 'event.target' (The clicked button)
-
-//  > var theClickedHour = $(event.target).data("hour");
-
-//  > var value = $(event.target).prev().val()
-
-//  > localStorage.setItem( "hour-"+ theClickedHour, value );
